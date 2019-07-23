c3d82d1bfae86cb075061244669eabffd71c6631
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:34:33 -0400

    Handlers

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         main_template = jinja_env.get_templates("templates/mainpage.html")
         self.response.write(main_template.render())
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+], debug=True)
