3d5eea3f6a814ff1cfea0576b3ec359e91d98303
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:18:11 -0400

    File Containg Models

---
+++ models.py
@@ -0,0 +1,10 @@
+from google.appengine.ext import ndb
+
+
+class CRN(ndb.Model):
+    bld_num = ndb.StringProperty(required = True)
+    days = ndb.StringProperty(required = True, repeated = True)
+    times = ndb.DatetTimeProperty(required = True)
+
+class USER(ndb.Model):
+    courses = ndb.KeyProperty(CRN, repeated = True)
