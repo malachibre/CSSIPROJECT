6e6a311f971df27f4cdb70778a087bb088eb1b92
malachibre <cssi@cssis-MacBook-Pro.local>
2019-07-23 16:20:56 -0400

    push

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+buildings = {
+    '53': [33.9534, 83.3734],
+    '56': 'Park Hall',
+    '58': 'Sanford Hall',
+    '1033': 'Ecology',
+    '1013': 'Poultry Science',
+    '1040': 'Forestry Resources 1',
+    '1057': 'Davison Life Sciences Complex',
+    '1023': 'Boyd Graduate Research Center',
+    '1038': 'Pharmacy South',
+    '1020': 'Food Science Building',
+    '1061': 'Miller Plant Science',
+    '1033': 'Ecology',
+    '1031': 'Hardman Hall',
+    '1035': 'Science Learning Center'
+    '1010': 'Speirs Hall',
+    '1003': 'Physics Bulding',
+    '1060': 'Aderhold',
+    '1001': 'Chemistry',
+    '1041':  'R. C. Wilson Pharmacy'
+}
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
