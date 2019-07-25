6621bc02ad42465ad3103f2362070d5ffbb87835
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-25 14:56:37 -0400

    push

--- static/main.js
+++ static/main.js
@@ -12,5 +12,9 @@ const newInput = () => {
   // // document.body.appendChild(button2);
   // <!--- -->
 }
+window.onload=function(){
+  document.getElementById("button").style.display='none';
+
+}
 
 button = document.querySelector("#addCRN");
