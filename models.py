d1d1e1f7efad41568f6d1ed7d63cfccc8f64762e
malachibre <cssi@cssis-MacBook-Pro.local>
2019-07-25 14:31:36 -0400

    push

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from google.appengine.ext import ndb
+import jinja2
+import webapp2
 
 
 class CRN(ndb.Model):
     bld_num = ndb.StringProperty(required = True)
     days = ndb.StringProperty(required = True, repeated = True)
-    times = ndb.DatetTimeProperty(required = True)
+    times = ndb.DatetTimeProperty(required = False)
 
 class User(ndb.Model):
     courses = ndb.KeyProperty(CRN, repeated = True)
