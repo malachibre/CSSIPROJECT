0cd7cfb75d2ef632419c7d15496dfb3501b87872
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 16:25:32 -0400

    push

--- main.py
+++ main.py
@@ -16,42 +16,42 @@ class CRN(ndb.Model):
 class User(ndb.Model):
     courses = ndb.KeyProperty(CRN, repeated = True)
 
-
-C16043 = CRN(number = 16043, name = "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
-C16043.put()
-
-C25093 = CRN(number = 25093, name= "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
-C25093.put()
-
-C25094 = CRN(number = 25094, name= "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
-C25094.put()
-
-C15353 = CRN(number = 15353, name= "MATH1113", bld_num = 1033, days = ['t', 'r'])
-C15353.put()
-
-C15356 = CRN(number = 15356, name= "MATH1113", bld_num = 1013, days = ['m', 'w', 'f'])
-C15356.put()
-
-C15359 = CRN(number = 15359, name= "MATH1113", bld_num = 1040, days = ['m', 'w', 'f'])
-C15359.put()
-
-C22591 = CRN(number = 22591, name= "HIST2112", bld_num = 66, days = ['t', 'r'])
-C22591.put()
-
-C25894 = CRN(number = 25894, name= "HIST2112", bld_num = 66, days = ['m', 'w'])
-C25894.put()
-
-C42135 = CRN(number = 42135, name= "HIST2111", bld_num = 58, days = ['m', 'w'])
-C42135.put()
-
-C33739 = CRN(number = 33739, name="HIST2111", bld_num = 66, days = ['t', 'r'])
-C33739.put()
-
-C10065 = CRN(number = 10065, name= "BIOL1103", bld_num = 1035, days = ['t', 'r'])
-C10065.put()
-
-C10098 = CRN(number = 10098, name="BIOL1103", bld_num = 1035, days = ['m', 'w', 'f'])
-C10098.put()
+#
+# C16043 = CRN(number = 16043, name = "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
+# C16043.put()
+#
+# C25093 = CRN(number = 25093, name= "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
+# C25093.put()
+#
+# C25094 = CRN(number = 25094, name= "ENGL1101", bld_num = 56, days = ['m', 'w', 'f'])
+# C25094.put()
+#
+# C15353 = CRN(number = 15353, name= "MATH1113", bld_num = 1033, days = ['t', 'r'])
+# C15353.put()
+#
+# C15356 = CRN(number = 15356, name= "MATH1113", bld_num = 1013, days = ['m', 'w', 'f'])
+# C15356.put()
+#
+# C15359 = CRN(number = 15359, name= "MATH1113", bld_num = 1040, days = ['m', 'w', 'f'])
+# C15359.put()
+#
+# C22591 = CRN(number = 22591, name= "HIST2112", bld_num = 66, days = ['t', 'r'])
+# C22591.put()
+#
+# C25894 = CRN(number = 25894, name= "HIST2112", bld_num = 66, days = ['m', 'w'])
+# C25894.put()
+#
+# C42135 = CRN(number = 42135, name= "HIST2111", bld_num = 58, days = ['m', 'w'])
+# C42135.put()
+#
+# C33739 = CRN(number = 33739, name="HIST2111", bld_num = 66, days = ['t', 'r'])
+# C33739.put()
+#
+# C10065 = CRN(number = 10065, name= "BIOL1103", bld_num = 1035, days = ['t', 'r'])
+# C10065.put()
+#
+# C10098 = CRN(number = 10098, name="BIOL1103", bld_num = 1035, days = ['m', 'w', 'f'])
+# C10098.put()
 
 crnEntries = CRN.query()
 crnNumbers = []
@@ -87,7 +87,8 @@ buildings = {
 }
 
 template_dict = {
-    'courses': {}
+    'all_courses': CRN.query().fetch(),
+    'selected_courseList': []
 }
 
 class MainHandler(webapp2.RequestHandler):
@@ -97,17 +98,22 @@ class MainHandler(webapp2.RequestHandler):
 
 class RedirectHandler(webapp2.RequestHandler):
     def post(self):
-        for i in range(1,6):
-            key = "CRN" + str(i)
-            num = self.request.get(key)
-            if(len(str(num)) > 0):
-                course = CRN.query().filter(CRN.number == int(num)).fetch()[0]
-                template_dict['courses'][key] = [course.name, buildings[str(course.bld_num)], course.days, course.number]
-        for course in template_dict['courses']:
-            self.response.write(template_dict['courses'][course][3])
+        all_courses = template_dict['all_courses']
+        selected = []
+        for course in all_courses:
+            key = "CRN" + str(course.number)
+            if self.request.get(key):
+
+                # course = CRN.query().filter(CRN.number == int(c.number)).fetch()[0]
+                # template_dict['courses'][key] = [course.name, buildings[str(course.bld_num)], course.days, course.number]
+                selected.append(course)
+                print(key)
+        # for course in template_dict['courses']:
+        #     self.response.write(template_dict['courses'][course][3])
 
         redirect_template = jinja_env.get_template("templates/AddedCourse.html")
-        self.response.write(redirect_template.render())
+        template_dict['selected_courseList'] = selected
+        self.response.write(redirect_template.render(template_dict))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
